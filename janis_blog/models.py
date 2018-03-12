d4cdc2783dc42b84cffbf8b128d092c965f21605
Janis <janis03050055@gmail.com>
2018-03-12 19:25:25 +0800

    添加POST模型及資料表

--- janis_blog/models.py
+++ janis_blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+# models.Model 用來定義Django模型，POST為模型的名字
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    #blank=True，允許輸入空值
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+    picture = models.URLField(blank=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
